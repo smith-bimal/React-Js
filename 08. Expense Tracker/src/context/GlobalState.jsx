562241fe02b0dbefe89847136d72a28d5a7c8607
smith-bimal <{golokbihareebeemal@gmail.com}>
2024-10-05 15:24:16 +0530

    New Expense Tracker Project Added

---
+++ 08. Expense Tracker/src/context/GlobalState.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer"
+
+//Intial state
+const initialState = {
+  transactions: [],
+};
+
+//Create Context
+export const GlobalContext = createContext(initialState);
+
+//Provider Component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //Actions
+  const deleteTransaction = (id)=>{
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    })
+  }
+
+  const addTransaction = (transaction)=> {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction
+    })
+  }
+
+  return(
+    <GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>
+  )
+};
